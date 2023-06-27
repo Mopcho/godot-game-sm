c52a3b9f87d536659f2a82d65e200f37770923af
MopchoNex <mopchowork@gmail.com>
2023-06-27 15:50:25 +0300

    Extract the animation into a animation player

--- Player/Player.gd
+++ Player/Player.gd
@@ -7,9 +7,7 @@ const JUMP_VELOCITY = -400.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-
-func _ready():
-	get_node("AnimatedSprite2D").play("Idle");
+@onready var anim = get_node("AnimatedSprite2D")
 
 func _physics_process(delta):
 	# Add the gravity.
