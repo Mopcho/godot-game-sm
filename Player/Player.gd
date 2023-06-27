3939d3154816e97b93c3349513ca2e4983d273c3
MopchoNex <mopchowork@gmail.com>
2023-06-27 15:30:28 +0300

    Add idle animation on ready

--- Player/Player.gd
+++ Player/Player.gd
@@ -8,6 +8,9 @@ const JUMP_VELOCITY = -400.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
+func _ready():
+	get_node("AnimatedSprite2D").play("Idle");
+
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
