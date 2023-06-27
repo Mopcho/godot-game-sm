6aed1d776c3ca4202d5ed594674a76f3b7fe343e
MopchoNex <mopchowork@gmail.com>
2023-06-28 01:18:44 +0300

    Parallel tweens

--- Collectables/Cherry.gd
+++ Collectables/Cherry.gd
@@ -5,6 +5,8 @@ extends Area2D
 func _on_body_entered(body):
 	if body.name == "Player":
 		Game.playerGold += 5
-		var tween = get_tree().create_tween()
-		tween.tween_property(self, "position", position - Vector2(0, 50), 1)
-		tween.tween_callback(queue_free)
+		var tween1 = get_tree().create_tween()
+		var tween2 = get_tree().create_tween()
+		tween1.tween_property(self, "position", position - Vector2(0, 50), 1)
+		tween2.tween_property(self, "modulate:a", 0, 1)
+		tween1.tween_callback(queue_free)
