c25d6e22814f0003af79f2d94635a1729ec36270
MopchoNex <mopchowork@gmail.com>
2023-06-27 16:07:38 +0300

    Flip sprite on facing direction

--- Player/Player.gd
+++ Player/Player.gd
@@ -22,6 +22,11 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction == -1:
+		get_node("AnimatedSprite2D").flip_h = true
+	elif direction == 1:
+		get_node("AnimatedSprite2D").flip_h = false
+	
 	if direction:
 		velocity.x = direction * SPEED
 		if velocity.y == 0:
