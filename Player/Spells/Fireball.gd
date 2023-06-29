2e1016b95623b915098f466588fd263f796712f3
MopchoNex <mopchowork@gmail.com>
2023-06-29 12:53:32 +0300

    Better fireball animations

--- Player/Spells/Fireball.gd
+++ Player/Spells/Fireball.gd
@@ -2,13 +2,19 @@ extends CharacterBody2D
 
 var new_velocity = Vector2(0, 0)
 var speed = 300
+var collided
 
 func _physics_process(delta):
-	get_node("AnimatedSprite2D").play("Idle")
-	move_and_collide(new_velocity.normalized() * delta * speed)
+	if !collided:
+		get_node("AnimationPlayer").play("Idle")
+		move_and_collide(new_velocity.normalized() * delta * speed)
 
 func _on_deal_damage_body_entered(body):
 	if body.is_in_group("mobs"):
 		body.take_damage(10)
+	collided = true
+	var anim_player = get_node("AnimationPlayer")
+	anim_player.play("Boom")
+	await anim_player.animation_finished
 	queue_free()
 
