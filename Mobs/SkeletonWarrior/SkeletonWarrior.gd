a239918f2d1d7c336b7f5225909a3ec2b86ef82c
MopchoNex <mopchowork@gmail.com>
2023-06-30 01:47:58 +0300

    Add new components and remove old functionality

--- Mobs/SkeletonWarrior/SkeletonWarrior.gd
+++ Mobs/SkeletonWarrior/SkeletonWarrior.gd
@@ -1,50 +1,9 @@
-extends BaseMob
+extends CharacterBody2D
 
 var animationPlayer: AnimationPlayer
 
 func _ready():
-	level = 2
-	health = 20
-	max_health = 20
-	speed = 100
-	animationPlayer = get_node("AnimationPlayer")
-	animationPlayer.play("Idle")
-	super()
+	pass
 
 func _physics_process(delta):
-	super(delta)
-	
-	if dead || attacking:
-		return
-	elif chase:
-		animationPlayer.play("Run")
-	else:
-		animationPlayer.play("Idle")
-
-func before_death():
-	super()
-	animationPlayer.play("Death")
-	await animationPlayer.animation_finished
-
-func _on_player_detection_body_entered(body):
-	if body.name == "Player":
-		chase = true
-
-func _on_player_damage_body_entered(body):
-	if body.name == "Player":
-		attacking = true
-		animationPlayer.play("Attack")
-
-func _on_player_damage_body_exited(body):
-	if body.name == "Player":
-		attacking = false
-		
-func _on_animation_player_animation_finished(anim_name):
-	if anim_name == "Attack":
-		Player.health -= damage
-		if attacking:
-			animationPlayer.play("Attack")
-
-func _on_player_max_chase_zone_body_exited(body):
-	if body.name == "Player":
-		chase = false
+	pass
